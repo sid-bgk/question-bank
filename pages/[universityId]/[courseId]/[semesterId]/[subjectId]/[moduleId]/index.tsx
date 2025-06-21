0201a113bc8f4781de51a259784656bd5cf36b62
sid-bgk <siddheshx@gmail.com>
2025-06-21 21:04:14 +0530

    fixed lint issue

--- pages/[universityId]/[courseId]/[semesterId]/[subjectId]/[moduleId]/index.tsx
+++ pages/[universityId]/[courseId]/[semesterId]/[subjectId]/[moduleId]/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 import Sidebar from "../../../../../../components/Sidebar";
 import MCQ from "../../../../../../components/MCQ";
 import Brief from "../../../../../../components/Brief";
